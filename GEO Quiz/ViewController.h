a15efddd6740154e4c18f1af6181369b44949306
Dani <Boti@Boti.local>
2014-02-02 17:45:25 +0100

    Initial Commit

---
+++ GEO Quiz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GEO Quiz
+//
+//  Created by Dani on 02/02/14.
+//  Copyright (c) 2014 Dani Boti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GEO Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GEO Quiz
+//
+//  Created by Dani on 02/02/14.
+//  Copyright (c) 2014 Dani Boti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
